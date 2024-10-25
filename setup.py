eadf32eccbc8297430675972ac986f32f376c353
HugoPhi <hugonelson07@outlook.com>
2024-10-25 21:55:04 +0800

    release v0.1.4
    - Fixed Bugs:
      - fixed some confusion on some branches & commits

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='hym',
-    version='v0.1.2',
+    name='hym20220592',
+    version='v0.1.4',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -11,4 +14,6 @@ setup(
         'matplotlib',
         'pandas'
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # 如果 README.md 是 Markdown 格式
 )
